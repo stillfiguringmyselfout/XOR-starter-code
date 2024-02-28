61fc54208ce1b441873d7b056ba8567e22d14786
stillfiguringmyselfout <98859520+stillfiguringmyselfout@users.noreply.github.com>
2024-02-28 21:21:14 +0000

    In Class XOR

--- XORinClassProject.py
+++ XORinClassProject.py
@@ -1,36 +1,4 @@
-def XOR(bit1, bit2):
-    if bit1 == bit2:
-        return '0'
-    else:
-        return '1'
-
-def XORonByte(byte, key):
-    emsg = ""
-    for i in range(len(byte)):
-        emsg += XOR(byte[i], key[i])
-    return emsg
-
-print(XORonByte("0010", "0011")
-
-def XORonLetter(letter, keyLetter)
-
-    letterBin = encode(letter)
-    keyLetterBin = encode(keyLetter)
-
-    encryptedLetter = XORonByte(letterBin, keyLetterBin)
-
-    return decode(encryptedLetter)
-
-print(XORonLetter("d", "r"))
-
-def XORonSentence(sentence, key):
-    encryptedSentence = ""
-    for i in range(len(sentence)):
-        encryptedSentence += XORonLetter(sentence[i], key[i])
-    return encryptedSentence
-
-print(XORonSentence("hello", "world"))
-print(XORonSentence("rkAan", "world"))
+import math
 
 characters = [
     # lowercase characters
@@ -50,4 +18,73 @@ def encode(character):
 def decode(binary):
     charIndex = int(binary, 2)
     return characters[charIndex]
+
+def generateKey(message, key):
+
+    if len(message) == len(key):
+
+        return key
+    
+    elif len(message) == len(key):
+
+        return key[0 : len(message)]
+    
+    else:
+
+        genKey = ""
+        rep = math.floor(len(message)/len(key))
+        rem = len(message) % len(key)
+
+        for i in range(rep):
+            genKey += key
+
+        genKey += key[0 : rem]
+
+        return genKey
+    
+print(generateKey("mississippi","hip"))
+
+
+def XOR(bit1, bit2):
+    if bit1 == bit2:
+        return '0'
+    else:
+        return '1'
+
+def XORonByte(byte, key):
+    emsg = ""
+    for i in range(len(byte)):
+        emsg += XOR(byte[i], key[i])
+    return emsg
+
+print(XORonByte("0010", "0011"))
+
+def XORonLetter(letter, keyLetter):
+
+    letterBin = encode(letter)
+    keyLetterBin = encode(keyLetter)
+
+    encryptedLetter = XORonByte(letterBin, keyLetterBin)
+
+    return decode(encryptedLetter)
+
+print(XORonLetter("d", "r"))
+
+def XORonSentence(sentence, key):
+
+    encryptedSentence = ""
+    genKey = generateKey(sentence, key)
+
+    for i in range(len(sentence)):
+        encryptedSentence += XORonLetter(sentence[i], genKey[i])
+    return encryptedSentence
+
+print(XORonSentence("hello", "world"))
+print(XORonSentence("rkAan", "world"))
+print(XORonSentence("hello how are you","may"))
+
+msg = input("Enter a message: ")
+key = input("Enter a key: ")
+
+print("Your encrypted message is:", XORonSentence(msg, key))
 
